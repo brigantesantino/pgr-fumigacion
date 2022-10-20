7ba84356852f9a3cf1fdbbc0453aa563ea7831b4
Santino Brigante <sabrigante@ycrt.local>
2022-10-20 11:34:10 -0300

    heroku

--- app.js
+++ app.js
@@ -3,19 +3,10 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3001, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
-
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-//fds
